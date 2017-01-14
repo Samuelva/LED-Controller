344eb27f1b5797505c312fba8d194fc8e55a66df
samuelva <samuelvanapeldoorn@outlook.com>
2017-01-14 18:21:07 +0100

    Button should start the LED

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -1,8 +1,18 @@
+import boot
 import os
-from flask import Flask, session, g, redirect, url_for, abort, render_template, flash
+from flask import Flask, session, g, redirect, url_for, abort, render_template, flash, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def main():
+    if request.form == "POST":
+        print("lol")
+        # boot.boot(strip, Color(0, 0, 0, 255), 0)
     return(render_template("main.html"))
+
+
+# export FLASK_APP=flaskr
+# export FLASK_DEBUG=true
+# flask run
+
